24f7a2c9733c144597f0eac5e7e49dfd88c29adf
Leon3854 <frad109@mail.ru>
2024-09-25 21:26:03 +0300

    Finished working with component auth

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,13 @@
 import { Module } from '@nestjs/common'
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
-import { UserModule } from './user/user.module';
-import { AuthModule } from './auth/auth.module';
-import { StatisticsController } from './statistics/statistics.controller';
-import { StatisticsModule } from './statistics/statistics.module';
+import { UserModule } from './user/user.module'
+import { AuthModule } from './auth/auth.module'
+import { StatisticsModule } from './statistics/statistics.module'
 
 @Module({
   imports: [UserModule, AuthModule, StatisticsModule],
-  controllers: [AppController, StatisticsController],
+  controllers: [AppController],
   providers: [AppService]
 })
 export class AppModule {}
