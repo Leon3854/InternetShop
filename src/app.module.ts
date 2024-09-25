b15e058d1e781e4a73172224dcb06738b7a76abc
Leon3854 <frad109@mail.ru>
2024-09-25 09:31:59 +0300

    Working with statistics component

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,12 @@ import { AppController } from './app.controller'
 import { AppService } from './app.service'
 import { UserModule } from './user/user.module';
 import { AuthModule } from './auth/auth.module';
+import { StatisticsController } from './statistics/statistics.controller';
+import { StatisticsModule } from './statistics/statistics.module';
 
 @Module({
-  imports: [UserModule, AuthModule],
-  controllers: [AppController],
+  imports: [UserModule, AuthModule, StatisticsModule],
+  controllers: [AppController, StatisticsController],
   providers: [AppService]
 })
 export class AppModule {}
