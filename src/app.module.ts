56029d4ad542f2ea326221d77e055097c0318672
Leon3854 <frad109@mail.ru>
2024-09-26 10:29:52 +0300

    Working with the component pagination

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,10 @@ import { UserModule } from './user/user.module'
 import { AuthModule } from './auth/auth.module'
 import { StatisticsModule } from './statistics/statistics.module'
 import { CategoryModule } from './category/category.module'
+import { PaginationModule } from './pagination/pagination.module';
 
 @Module({
-  imports: [UserModule, AuthModule, StatisticsModule, CategoryModule],
+  imports: [UserModule, AuthModule, StatisticsModule, CategoryModule, PaginationModule],
   controllers: [AppController],
   providers: [AppService]
 })
